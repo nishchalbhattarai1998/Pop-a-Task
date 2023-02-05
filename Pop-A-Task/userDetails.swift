088b5499e12d75902674007e5f711f00b5b04c1a
Harneet Kaur <harneet.layal@hotmail.com>
2023-02-05 17:27:30 -0500

    editable fields in the userDetails views #27

--- Pop-A-Task/userDetails.swift
+++ Pop-A-Task/userDetails.swift
@@ -9,10 +9,42 @@ import Foundation
 import SwiftUI
 
 struct userDetails: View {
-    var body: some View {
-        Text("Welcome")
+    @State private var name: String = "John Doe"
+        @State private var email: String = "john.doe@example.com"
+        @State private var isEditing = false
+
+        var body: some View {
+            VStack {
+                if isEditing {
+                    Form {
+                        Section {
+                            TextField("Name", text: $name)
+                            TextField("Email", text: $email)
+                        }
+                    }
+                } else {
+                    Form {
+                        Section {
+                            Text("Name: \(name)")
+                            Text("Email: \(email)")
+                        }
+                    }
+                }
+                HStack {
+                    if isEditing {
+                        Button("Save") {
+                            self.isEditing = false
+                        }
+                    } else {
+                        Button("Edit") {
+                            self.isEditing = true
+                        }
+                    }
+                }
+            }
+        }
     }
-}
+
 
 struct userDetails_Previews: PreviewProvider {
     static var previews: some View {
