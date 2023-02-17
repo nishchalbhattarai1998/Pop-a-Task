b7cb619590bbd468f26e2a2b1f92828848dfe30e
charles-mopr <111164991+charles-mopr@users.noreply.github.com>
2023-02-17 11:55:38 -0500

    Added animation to the search field tap action issue #8

--- Pop-A-Task/Groups/GroupViewS.swift
+++ Pop-A-Task/Groups/GroupViewS.swift
@@ -55,7 +55,7 @@ struct GroupView: View {
             .onChange(of: viewModel.searchTerm) { newValue in
                 viewModel.filterSearchResults()
             }
-            .animation(.easeInOut, value: viewModel.searchTerm)
+            .animation(.default, value: viewModel.searchTerm)
 
             // Toolbar: Add and Edit
             .toolbar {
@@ -96,91 +96,6 @@ struct GroupView: View {
     }
 
 }
-
-/// Contact list view
-//struct GroupView: View {
-//    @ObservedObject var viewModel: GroupViewModel
-//    @State private var isShowingModal = false
-//    let db = Firestore.firestore()
-//
-//    var body: some View {
-//
-//        NavigationView {
-//
-//            List {
-//                ForEach(viewModel.listData) { group in
-//                    GroupRow(group: group)
-//                }
-//                .onMove(perform: moveGroup)
-//                .onDelete(perform: deletGroup)
-//
-//                HStack {
-//                    Spacer()
-//                    Text(viewModel.displayCount)
-//                        .foregroundColor(.gray)
-//
-//                    Spacer()
-//                }
-//            }
-//            .navigationTitle(viewModel.navTitle)
-//            // To Add search capability
-//            .searchable(text: $viewModel.searchTerm,
-//                        placement: .navigationBarDrawer(displayMode: .automatic),
-//                        prompt: "Search for groups")
-//            .onChange(of: viewModel.searchTerm, perform: { newValue in viewModel.filterSearchResults()
-//            }
-//            )
-//            .animation(.default, value: viewModel.searchTerm)
-//            // Toolbar: Add and Edit
-//            .toolbar {
-//                HStack {
-//                    Button("Add"){
-//                        isShowingModal = true
-//
-//                    }.sheet(isPresented: $isShowingModal) {
-//                        ModalView(isShowingModal: $isShowingModal)
-//                            .cornerRadius(20)
-////                            .padding(50)
-////                            .shadow(radius: 20)
-////                            .background(Color.gray)
-//                    }
-//                        Spacer()
-//                        EditButton()
-//                        Button("Reset", action: resetData)
-//
-//                }
-//            }
-//        }}
-//
-//    func resetData() {
-//        viewModel.resetData()
-//    }
-//
-////
-////    func addGroups() {
-////        withAnimation {
-////            guard let randomContact = viewModel.store.groups.randomElement() else {
-////                return
-////            }
-////            viewModel.addGroup(contact: randomContact)
-////        }
-////    }
-//
-//
-//
-//    func moveGroup(from: IndexSet, to: Int) {
-//        withAnimation {
-//            viewModel.moveGroup(from: from, to: to)
-//        }
-//    }
-//
-//
-//    func deletGroup(offsets: IndexSet) {
-//        withAnimation {
-//            viewModel.deleteGroup(offsets: offsets)
-//        }
-//    }
-//}
 
 struct GroupView_Previews: PreviewProvider {
     static var previews: some View {
