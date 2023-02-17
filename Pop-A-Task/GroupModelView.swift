924ea0a7d7c551b48c1de9851106c4e6c7c9ac6f
Gurungsangam <s_gurung168388@fanshaweonline.ca>
2023-02-17 01:59:02 -0500

    created add group Modal View #29

---
+++ Pop-A-Task/GroupModelView.swift
@@ -0,0 +1,81 @@
+
+//
+//  GroupModalView.swift
+//  Pop-A-Task
+//
+//  Created by nishchal bhattarai on 2023-02-17.
+//
+
+import Foundation
+import SwiftUI
+struct ModalView: View {
+    @Binding var isShowingModal: Bool
+    @State var groupName = ""
+    @State var description = ""
+    @State  var selectedOption: Int 
+    let options = ["Option 1", "Option 2", "Option 3"]
+    var body: some View {
+        VStack {
+            HStack {
+                //                Text("This is Group modal view")
+                //                    .font(.largeTitle)
+                //                    .padding()
+                Text("Add Task").font(.headline)
+                    .font(.headline)
+                    .padding()
+                Spacer()
+                Button(action: {
+                    isShowingModal = false
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.gray)
+                        .padding()
+                }
+            }
+            Divider()
+            
+            
+            TextField("Name of the group", text: $groupName)
+                .padding()
+                .background(Color(hue: 0.345, saturation: 0.095, brightness: 0.952))
+                .cornerRadius(15.0)
+//                        Spacer()
+            Divider()
+            TextField("Description of the group", text: $description).frame( width: 400, height: 50)
+                .padding()
+                .background(Color(hue: 0.345, saturation: 0.095, brightness: 0.952))
+                .cornerRadius(15.0)
+            Divider()
+//            VStack{
+                Picker("Select an option", selection: $selectedOption) {
+                    ForEach(0 ..< options.count) {i in
+                        Text(options[i]).onTapGesture {
+                            selectedOption = i
+                    }
+                    }
+                }.pickerStyle(MenuPickerStyle())
+                
+                Text("You selected: \(options[selectedOption])")
+                Button("Dismiss") {
+                    isShowingModal = false
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+//            }
+//            .padding()
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .cornerRadius(20)
+//            .shadow(radius: 20)
+        }
+    }
+}
+    struct ModalView_Previews: PreviewProvider {
+        static var previews: some View {
+            ModalView(isShowingModal: .constant(true), selectedOption: 0)
+        }
+    }
+
--- Pop-A-Task/Groups/GroupViewS.swift
+++ Pop-A-Task/Groups/GroupViewS.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import FirebaseFirestore
+
 struct GroupViews: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @Binding var isLoggedIn: Bool
@@ -23,7 +23,8 @@ struct GroupViews: View {
 /// Contact list view
 struct GroupView: View {
     @ObservedObject var viewModel: GroupViewModel
-    let db = Firestore.firestore()
+    @State private var isShowingModal = false
+    
     var body: some View {
         
         NavigationView {
@@ -41,7 +42,7 @@ struct GroupView: View {
                     Spacer()
                     Text(viewModel.displayCount)
                         .foregroundColor(.gray)
- 
+                    
                     Spacer()
                 }
             }
@@ -57,30 +58,43 @@ struct GroupView: View {
             // Toolbar: Add and Edit
             .toolbar {
                 HStack {
-                    Button("Add", action: addGroup)
-                    Spacer()
-                    EditButton()
-                    Button("Reset", action: resetData)
+                    Button("Add"){
+                        isShowingModal = true
+                        
+                    }.sheet(isPresented: $isShowingModal) {
+                        ModalView(isShowingModal: $isShowingModal, selectedOption: 0)
+                            .cornerRadius(20)
+//                            .padding(50)
+//                            .shadow(radius: 20)
+//                            .background(Color.gray)
+                    }
+                        Spacer()
+                        EditButton()
+                        Button("Reset", action: resetData)
+                    
                 }
             }
-        }
-    }
+        }}
     
     func resetData() {
         viewModel.resetData()
     }
     
    
-    func addGroup() {
-        let group = Groups(name: "New Group", description: "", members: [], createDate: Date())
-        do {
-            let _ = try db.collection("groups").addDocument(from: group)
-            print("Group added successfully to Firestore")
-        } catch let error {
-            print("Error adding group to Firestore: \(error.localizedDescription)")
+    func makeContact() {
+        withAnimation {
+            guard let randomContact = viewModel.store.groups.randomElement() else {
+                return
+            }
+            viewModel.makeContact(contact: randomContact)
         }
     }
-
+    
+    func addGroup(){
+        
+        
+        
+    }
     
     
     func moveContacts(from: IndexSet, to: Int) {
