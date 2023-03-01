5cea08e758cfed60f6f5a5895d561f9e014f911e
Manpreet-kau <108590534+Manpreet-kau@users.noreply.github.com>
2023-02-28 19:35:51 -0500

    fixing ui bugs at group details view issue #30

--- Pop-A-Task/Groups/GroupDetail.swift
+++ Pop-A-Task/Groups/GroupDetail.swift
@@ -29,129 +29,244 @@ struct GroupDetail: View {
     }()
     
     var body: some View {
-        VStack {
-            Text(group.name)
-                .font(.system(size: 20))
-                .fontWeight(.semibold)
-                .padding(.vertical, 15)
-                .frame(height:25.0)
-            
-            
-            
-            List {
-                Section(header: Text("Group Details")) {
-                    VStack(alignment: .leading) {
-                        Text(group.description)
-                            .font(.system(size: 15))
-                            .font(.title)
-                            .multilineTextAlignment(.leading)
-                            .padding(.bottom)
+//        NavigationView {
+//            VStack{
+                List {
+                    Section(header: Text("Group Details")) {
+                        VStack(alignment: .leading) {
+                            VStack{
+                                Text(group.description)
+                                    .font(.system(size: 15))
+                                    .font(.title)
+                                    .multilineTextAlignment(.leading)
+                                    .padding(.bottom)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .padding()
+                            .cornerRadius(20)
+                            .background(.gray)
+                            .opacity(0.1)
+                            
+                            HStack {
+                                
+                                    Text("Created By: \(group.createBy)")
+                                        .font(.footnote)
+                                        .fontWeight(.light)
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.leading)
+                              
+                                Spacer()
+                                Text(dateFormatter.string(from: group.createDate))
+                                    .font(.footnote)
+                                    .fontWeight(.light)
+                                    .foregroundColor(.gray)
+                                    .multilineTextAlignment(.trailing)
+                            }
                             .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        HStack {
-                            Text("Created By: \(group.createBy)")
-                                .font(.footnote)
-                                .fontWeight(.light)
-                                .foregroundColor(.gray)
-                                .multilineTextAlignment(.leading)
-                            Spacer()
-                            Text(dateFormatter.string(from: group.createDate))
-                                .font(.footnote)
-                                .fontWeight(.light)
-                                .foregroundColor(.gray)
-                                .multilineTextAlignment(.trailing)
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
                     }
-                }
-                
-                HStack {
-                    Text("Members")
-                    Spacer()
-                    Button("Add Member") {
-                        isMemberModal = true
-                    }.foregroundColor(.blue)
+                    .listRowInsets(EdgeInsets())
+                    
+                    Section(header: HStack {
+                        Text("Members")
+                        Spacer()
+                        Button("Add Member") {
+                            isMemberModal = true
+                        }
+                        .foregroundColor(.blue)
                         .sheet(isPresented: $isMemberModal) {
                             MemberModalView(isShowingModal: $isMemberModal, id: group.id ?? "not found")
-                            .cornerRadius(20)
-                    }
-                }
-                    VStack{
+                                .cornerRadius(20)
+                        }
+                    }) {
                         ForEach(group.members, id: \.self) { member in
                             Text(member)
-                                .font(.subheadline)
-                                .padding(.bottom, 1.0)
-                            
+                                .padding(.vertical, 8)
+//                            Spacer()
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        Spacer()
+//                        .padding()
                     }
+                    .padding()
+                    .listRowInsets(EdgeInsets())
                     
-                    HStack {
+                    Section(header: HStack {
                         Text("Task")
                         Spacer()
-                        Button("Add Task") {
-                            isTaskModal = true
-                        }.sheet(isPresented: $isTaskModal){
-//                            AddTaskModalView(isTaskModal: $isTaskModal)
-                        }.foregroundColor(.blue)
+                        NavigationLink(destination: EmptyView()) {
+                            Text("Add Task")
+                        }
+                    }) {
+                        // Display tasks
+                    }
+                    .listRowInsets(EdgeInsets())
+                    
+                    Button("Permanently Delete Group") {
                         
+                        if
+                            userData.userName! == group.createBy {
+                            // Show confirmation message and delete on user confirm
+                            showAlert = true
+                            print("showAlert: \(showAlert)")
+                            print(userData.userName!)
+                            print(group.createBy)
+                        }
+                        //                else {
+                        //                    // Show message that user does not have permission
+                        //                    print("showAlert: \(showNoPermissionAlert)")
+                        //                    showNoPermissionAlert = true
+                        //
+                        //                    print(userData.userName!)
+                        //                    print(group.createBy)
+                        //                }
                     }
                     
+                    //            .alert(isPresented: $showNoPermissionAlert) {
+                    //                Alert(title: Text("Permission Denied"), message: Text("You do not have permission to delete this group."), dismissButton: .default(Text("OK")))
+                    //            }
                     
-                    
-                    
-                    
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Are you sure you want to delete this group?"), message: Text("All the tasks and activities conneced to this group will be also deleted. This action cannot be undone."), primaryButton: .destructive(Text("Delete")) {
+                            print("Deleted")
+                            viewModel.deleteGroup(group.id!)
+                        }, secondaryButton: .cancel())
+                    }
+                    .buttonStyle(.plain)
+                    .foregroundColor(.red)
+                    .padding(.leading, 50.0)
+//                    .background(Color.red)
+//                    .cornerRadius(50)
+                    .opacity(userData.userName != group.createBy ? 0.5 : 1.0) // Change opacity based on condition
+                    .disabled(userData.userName != group.createBy)
                 }
-                .listStyle(PlainListStyle())
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle(group.name)
+//                .padding(.vertical, 10)
+//                .background(Color.gray.opacity(0.1))
                 
-                
-            Button("Permanently Delete Group") {
-
-                if
-                   userData.userName! == group.createBy {
-                    // Show confirmation message and delete on user confirm
-                    showAlert = true
-                    print("showAlert: \(showAlert)")
-                    print(userData.userName!)
-                    print(group.createBy)
-                }
-//                else {
-//                    // Show message that user does not have permission
-//                    print("showAlert: \(showNoPermissionAlert)")
-//                    showNoPermissionAlert = true
+                //        VStack {
+                //            Text(group.name)
+                //                .font(.system(size: 20))
+                //                .fontWeight(.semibold)
+                //                .padding(.vertical, 15)
+                //                .frame(height:25.0)
+                //
+                //
+                //
+                //            List {
+                //                Section(header: Text("Group Details")) {
+                //                    VStack(alignment: .leading) {
+                //                        Text(group.description)
+                //                            .font(.system(size: 15))
+                //                            .font(.title)
+                //                            .multilineTextAlignment(.leading)
+                //                            .padding(.bottom)
+                //                            .frame(maxWidth: .infinity, alignment: .leading)
+                //
+                //                        HStack {
+                //                            Text("Created By: \(group.createBy)")
+                //                                .font(.footnote)
+                //                                .fontWeight(.light)
+                //                                .foregroundColor(.gray)
+                //                                .multilineTextAlignment(.leading)
+                //                            Spacer()
+                //                            Text(dateFormatter.string(from: group.createDate))
+                //                                .font(.footnote)
+                //                                .fontWeight(.light)
+                //                                .foregroundColor(.gray)
+                //                                .multilineTextAlignment(.trailing)
+                //                        }
+                //                        .frame(maxWidth: .infinity, alignment: .leading)
+                //                    }
+                //                }
+                //
+                //                HStack {
+                //                    Text("Members")
+                //                    Spacer()
+                //                    Button("Add Member") {
+                //                        isMemberModal = true
+                //                    }.foregroundColor(.blue)
+                //                        .sheet(isPresented: $isMemberModal) {
+                //                            MemberModalView(isShowingModal: $isMemberModal, id: group.id ?? "not found")
+                //                            .cornerRadius(20)
+                //                    }
+                //                }
+                //                VStack{
+                //                    List{
+                //                        ForEach(group.members, id: \.self) { member in
+                //                            Text(member)
+                //                        }
+                //
+                //                    }
+                //                }.font(.subheadline)
+                //                    .padding(.bottom, 1.0)
+                //                    .frame(maxWidth: .infinity, alignment: .leading)
+                //                    Spacer()
+                //                    HStack {
+                //                        Text("Task")
+                //                        Spacer()
+                //                        Button("Add Task") {
+                //                            isTaskModal = true
+                //                        }.sheet(isPresented: $isTaskModal){
+                ////                            AddTaskModalView(isTaskModal: $isTaskModal)
+                //                        }.foregroundColor(.blue)
+                //
+                //                    }
+                //
+                //
+                //
+                //
+                //
+                //                }
+                //                .listStyle(PlainListStyle())
+                //
+//            }
+//            Button("Permanently Delete Group") {
 //
+//                if
+//                    userData.userName! == group.createBy {
+//                    // Show confirmation message and delete on user confirm
+//                    showAlert = true
+//                    print("showAlert: \(showAlert)")
 //                    print(userData.userName!)
 //                    print(group.createBy)
 //                }
-            }
-
-//            .alert(isPresented: $showNoPermissionAlert) {
-//                Alert(title: Text("Permission Denied"), message: Text("You do not have permission to delete this group."), dismissButton: .default(Text("OK")))
+//                //                else {
+//                //                    // Show message that user does not have permission
+//                //                    print("showAlert: \(showNoPermissionAlert)")
+//                //                    showNoPermissionAlert = true
+//                //
+//                //                    print(userData.userName!)
+//                //                    print(group.createBy)
+//                //                }
 //            }
+//
+//            //            .alert(isPresented: $showNoPermissionAlert) {
+//            //                Alert(title: Text("Permission Denied"), message: Text("You do not have permission to delete this group."), dismissButton: .default(Text("OK")))
+//            //            }
+//
+//            .alert(isPresented: $showAlert) {
+//                Alert(title: Text("Are you sure you want to delete this group?"), message: Text("All the tasks and activities conneced to this group will be also deleted. This action cannot be undone."), primaryButton: .destructive(Text("Delete")) {
+//                    print("Deleted")
+//                    viewModel.deleteGroup(group.id!)
+//                }, secondaryButton: .cancel())
+//            }
+//            .buttonStyle(.plain)
+//            .foregroundColor(.white)
+//            .padding()
+//            .background(Color.red)
+//            .cornerRadius(50)
+//            .opacity(userData.userName != group.createBy ? 0.5 : 1.0) // Change opacity based on condition
+//            .disabled(userData.userName != group.createBy)
             
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Are you sure you want to delete this group?"), message: Text("All the tasks and activities conneced to this group will be also deleted. This action cannot be undone."), primaryButton: .destructive(Text("Delete")) {
-                    print("Deleted")
-                    viewModel.deleteGroup(group.id!)
-                }, secondaryButton: .cancel())
-            }
-            .buttonStyle(.plain)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.red)
-            .cornerRadius(50)
-            .opacity(userData.userName != group.createBy ? 0.5 : 1.0) // Change opacity based on condition
-            .disabled(userData.userName != group.createBy)
-            
-            }
-            .environmentObject(viewModel)
-//            .onAppear {
-//                viewModel.group = group
-//                viewModel.fetchUsers()
-            }
-        }
-
+//        }
+        .environmentObject(viewModel)
+        //            .onAppear {
+        //                viewModel.group = group
+        //                viewModel.fetchUsers()
+    }
+    
+}
 
 struct GroupDetail_Previews: PreviewProvider {
     static var previews: some View {
